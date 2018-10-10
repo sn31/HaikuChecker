fca41d8b07fb3ab475fdf3bb13cd32b38f22f955
Hyewon Cho and Skye Nguyen <hyewon@>
2018-10-10 16:20:56 -0700

    Add separateSuffix.

--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -5,4 +5,7 @@ describe("Haiku test", function() {
   it("should insert a dash betweeen prefix and the remaining of the word.", function() {
     expect(haikuInstance.separatePrefix("circumstance")).toEqual("circum-stance");
   });
+  it("should insert a dash before suffix.", function() {
+    expect(haikuInstance.separateSuffix("endless")).toEqual("end-less")
+  });
 });
--- src/haiku.js
+++ src/haiku.js
@@ -1,6 +1,6 @@
 const prefixes = ["ante", "anti", "circum", "co", "de", "dis", "em", "en", "epi", "ex", "extra", "fore", "homo", "hyper", "il", "im", "in", "ir", "infra", "inter", "intra", "macro", "micro", "mid", "mis", "mono", "non", "omni", "para", "post", "pre", "re", "semi", "sub", "super", "therm", "trans", "tri", "un", "uni"].sort(function(a, b) {return b.length-a.length});
 
-// const suffixes = ["acy", "al", "ance", "ence", "dom", "er", "or", "ism", "ist", "ity", "ty", "ment", "ness", "ship", "sion", "tion", "ate", "en", "ify", "fy", "ise", "ize", "able", "ible", "al", "esque", "ful", "ic", "ical", "ious", "ous", "ish", "ive", "less", "y", "ly", "ward", "wards", "wise"].sort(function(a, b) {return b.length-a.length});
+const suffixes = ["acy", "al", "ance", "ence", "dom", "er", "or", "ism", "ist", "ity", "ty", "ment", "ness", "ship", "sion", "tion", "ate", "en", "ify", "fy", "ise", "ize", "able", "ible", "al", "esque", "ful", "ic", "ical", "ious", "ous", "ish", "ive", "less", "y", "ly", "ward", "wards", "wise"].sort(function(a, b) {return b.length-a.length});
 
 export class Haiku {
   constructor(inputPoem) {
@@ -21,6 +21,20 @@ export class Haiku {
       let regex = new RegExp(prefix, "g");
       if (regex.test(word) == true) {
         newWord = word.substring(0, regex.lastIndex)+"-"+word.substring(regex.lastIndex);
+        return false;
+      }
+    });
+    return newWord;
+  }
+  //endless
+  separateSuffix(word) {
+    let newWord = word;
+    suffixes.forEach(function(suffix) {
+      if (newWord !== word) return false;
+      let regex = new RegExp(suffix+"$", "g");
+      if (regex.test(word) == true) {
+        newWord = word.substring(0, regex.lastIndex-suffix.length)+"-"+suffix;
+        console.log(newWord);
         return false;
       }
     });
