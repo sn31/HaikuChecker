7c0b5bbfdfbda57aa9ed256c87beae9dbf80e9f8
Hyewon Cho and Skye Nguyen <hyewon+skye@>
2018-10-11 09:34:49 -0700

    Working on split two consonants

--- src/haiku.js
+++ src/haiku.js
@@ -1,6 +1,9 @@
-const prefixes = ["ante", "anti", "circum", "co", "de", "dis", "em", "en", "epi", "ex", "extra", "fore", "homo", "hyper", "il", "im", "in", "ir", "infra", "inter", "intra", "macro", "micro", "mid", "mis", "mono", "non", "omni", "para", "post", "pre", "re", "semi", "sub", "super", "therm", "trans", "tri", "un", "uni"].sort(function(a, b) {return b.length-a.length});
+const prefixes = ["ante", "anti", "circum", "co", "de", "dis", "em", "en", "epi", "ex", "extra", "fore", "homo", "hyper", "il", "im", "in", "ir", "infra", "inter", "intra", "macro", "micro", "mid", "mis", "mono", "non", "omni", "para", "post", "pre", "re", "semi", "sub", "super", "therm", "trans", "tri", "un", "uni"].sort(function (a, b) { return b.length - a.length });
 
-const suffixes = ["acy", "al", "ance", "ence", "dom", "er", "or", "ism", "ist", "ity", "ty", "ment", "ness", "ship", "sion", "tion", "ate", "en", "ify", "fy", "ise", "ize", "able", "ible", "al", "esque", "ful", "ic", "ical", "ious", "ous", "ish", "ive", "less", "y", "ly", "ward", "wards", "wise"].sort(function(a, b) {return b.length-a.length});
+const suffixes = ["acy", "al", "ance", "ence", "dom", "er", "or", "ism", "ist", "ity", "ty", "ment", "ness", "ship", "sion", "tion", "ate", "en", "ify", "fy", "ise", "ize", "able", "ible", "al", "esque", "ful", "ic", "ical", "ious", "ous", "ish", "ive", "less", "y", "ly", "ward", "wards", "wise"].sort(function (a, b) { return b.length - a.length });
+
+const twoConsonantsException = ["ght", "th", "sh", "ph", "th", "ch", "wh", "gh"]; //if next letter is h then do nothing
+const vowelsList = ["a", "e", "i", "o", "u"]; //ignore y for now
 
 export class Haiku {
   constructor(inputPoem) {
@@ -16,11 +19,11 @@ export class Haiku {
   }
   separatePrefix(word) {
     let newWord = word;
-    prefixes.forEach(function(prefix) {
+    prefixes.forEach(function (prefix) {
       if (newWord !== word) return false;
       let regex = new RegExp(prefix, "g");
       if (regex.test(word) == true) {
-        newWord = word.substring(0, regex.lastIndex)+"-"+word.substring(regex.lastIndex);
+        newWord = word.substring(0, regex.lastIndex) + "-" + word.substring(regex.lastIndex);
         return false;
       }
     });
@@ -29,15 +32,40 @@ export class Haiku {
   //endless
   separateSuffix(word) {
     let newWord = word;
-    suffixes.forEach(function(suffix) {
+    suffixes.forEach(function (suffix) {
       if (newWord !== word) return false;
-      let regex = new RegExp(suffix+"$", "g");
+      let regex = new RegExp(suffix + "$", "g");
       if (regex.test(word) == true) {
-        newWord = word.substring(0, regex.lastIndex-suffix.length)+"-"+suffix;
+        newWord = word.substring(0, regex.lastIndex - suffix.length) + "-" + suffix;
         console.log(newWord);
         return false;
       }
     });
     return newWord;
   }
+
+  splitTwoConsonants(word) {
+    let newWord = word;
+    twoConsonantsException.forEach(function(exception) {
+      let matchingSubstrings = word.match(new RegExp(exception, "g"));
+    });
+    for (let i = 0; i < word.length - 1; i++) {
+      if (twoConsonantsException.includes(word.substring(i, i+2)) && word[i + 1] == "h") {
+        continue;
+      }
+      else if (twoConsonantsException.includes(word[i]) && !vowelsList.includes(word[i + 1])) {
+        //split
+        newWord = word.substring(0, i + 1) + "-" + word.substring(i + 1);
+      }
+      // if the consonant does not belong to the exceptions list and if they are vowels
+      else if (!vowelsList.includes(word[i]) && !vowelsList.includes(word[i + 1])) {
+        newWord = word.substring(0, i + 1) + "-" + word.substring(i + 1);
+      }
+      else {
+
+
+      }
+    }
+  }
+
 }
